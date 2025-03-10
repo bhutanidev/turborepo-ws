47600c8c8388bfe07bd06747b33fa3a721b8bffc
DEV BHUTANI <devbhutani10@gmail.com>
2025-03-11 00:51:41 +0530

    Initial Set Up http ws and fe

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@workspace/ui/components/button"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@workspace/ui/components/card"
 
 export default function Page() {
   return (
@@ -7,6 +8,22 @@ export default function Page() {
         <h1 className="text-2xl font-bold">Hello World</h1>
         <Button size="sm">Button</Button>
       </div>
+      <div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+
+      </div>
     </div>
+
   )
 }
---
+++ apps/ws-backend/src/index.ts
@@ -0,0 +1,10 @@
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 3001 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function message(data) {
+    ws.send("pong")
+    console.log("Inside message")
+  });
+});
